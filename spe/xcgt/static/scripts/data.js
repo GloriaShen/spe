491442ddad1a4abf50126ec9028c0d56c802bad5
Gloria <935360399@qq.com>
2017-11-30 16:05:29 +0800

    西城高铁，横屏展示

---
+++ spe/xcgt/static/scripts/data.js
@@ -0,0 +1,499 @@
+﻿var mockdata = [
+	{
+  
+		fname: '魏天露', tname: '孟梦寒'
+ 
+	},
+	{
+  
+		fname: '端木醉元', tname: '轩辕乐宜'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '雷绿薇'
+ 
+	},
+	{
+  
+		fname: '尉迟小卉', tname: '诸葛寻风'
+ 
+	},
+	{
+  
+		fname: '白巨炎', tname: '葛志宇'
+ 
+	},
+	{
+  
+		fname: '长孙初敏', tname: '王智云'
+
+	},
+	{
+  
+		fname: '龙求春', tname: '辛笑蕊'
+ 
+	},
+	{
+  
+		fname: '百里雪烟', tname: '杨近耀'
+ 
+	},
+	{
+  
+		fname: '乔亦和', tname: '项玉刚'
+ 
+	},
+	{
+  
+		fname: '东方丹之', tname: '林雨桃'
+ 
+	},
+	{
+  
+		fname: '柳华真', tname: '甘盼萍'
+ 
+	},
+	{
+  
+		fname: '张痴斌', tname: '唐安翔'
+ 
+	},
+	{
+  
+		fname: '关灵夜', tname: '华紫天'
+ 
+	},
+	{
+  
+		fname: '赵飞灵', tname: '曹慕荷'
+ 
+	},
+	{
+  
+		fname: '裴火山', tname: '敖凌翔'
+ 
+	},
+	{
+  
+		fname: '梁又爵', tname: '彭傲斐'
+ 
+	},
+	{
+  
+		fname: '包凡易', tname: '楚安彤'
+ 
+	},
+	{
+  
+		fname: '冯健空', tname: '魏问武'
+ 
+	},
+	{
+  
+		fname: '滕曼国', tname: '端木俊平'
+ 
+	},
+	{
+  
+		fname: '许惜山', tname: '田香竹'
+ 
+	},
+	{
+  
+		fname: '冷子丝', tname: '尉迟沛冬'
+ 
+	},
+	{
+  
+		fname: '吕苍佑', tname: '白霸顺'
+ 
+	},
+	{
+  
+		fname: '荆碧雁', tname: '长孙杰波'
+ 
+	},
+	{
+  
+		fname: '杜百芹', tname: '万晓容'
+ 
+	},
+	{
+  
+		fname: '上官义风', tname: '百里不之'
+ 
+	},
+	{
+  
+		fname: '方若忌', tname: '乔星刀'
+ 
+	},
+	{
+  
+		fname: '皇甫夏珊', tname: '东方之萱'
+ 
+	},
+	{
+  
+		fname: '秦光痕', tname: '柳青达'
+ 
+	},
+	{
+  
+		fname: '宇文冰福', tname: '张云康'
+ 
+	},
+	{
+  
+		fname: '段幼旋', tname: '关潇双'
+ 
+	},
+	{
+  
+		fname: '令狐叔白', tname: '黄怀丹'
+ 
+	},
+	{
+  
+		fname: '洪凌志', tname: '裴笑宁'
+ 
+	},
+	{
+  
+		fname: '西门雅槐', tname: '梁嘉柏'
+ 
+	},
+	{
+  
+		fname: '梅听蕾', tname: '包采筠'
+ 
+	},
+	{
+  
+		fname: '陈明哲', tname: '董迎松'
+ 
+	},
+	{
+  
+		fname: '苗映岳', tname: '滕清宝'
+ 
+	},
+	{
+  
+		fname: '周紫兰', tname: '许依蓉'
+ 
+	},
+	{
+  
+		fname: '霍留冰', tname: '卓傲睿'
+ 
+	},
+	{
+  
+		fname: '宋元邦', tname: '吕晓狂'
+ 
+	},
+	{
+  
+		fname: '柯元枫', tname: '荆森瑶'
+ 
+	},
+	{
+  
+		fname: '程千阳', tname: '杜寒梦'
+ 
+	},
+	{
+  
+		fname: '燕真怡', tname: '独孤静民'
+ 
+	},
+	{
+  
+		fname: '沈天翠', tname: '方辉蝶'
+ 
+	},
+	{
+  
+		fname: '花以雪', tname: '皇甫春云'
+ 
+	},
+	{
+  
+		fname: '蔡才轩', tname: '江诗墨'
+ 
+	},
+	{
+  
+		fname: '司马尘天', tname: '宇文立南'
+ 
+	},
+	{
+  
+		fname: '夏语凡', tname: '段劲夏'
+ 
+	},
+	{
+  
+		fname: '夏侯伯寒', tname: '令狐翠罡'
+ 
+	},
+	{
+  
+		fname: '熊逸强', tname: '鲁一阳'
+ 
+	},
+	{
+  
+		fname: '公孙绮菡', tname: '西门雪玉'
+ 
+	},
+	{
+  
+		fname: '魏天露', tname: '方若忌'
+ 
+	},
+	{
+  
+		fname: '端木醉元', tname: '皇甫夏珊'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '秦光痕'
+ 
+	},
+	{
+  
+		fname: '尉迟小卉', tname: '宇文冰福'
+ 
+	},
+	{
+  
+		fname: '白巨炎', tname: '段幼旋'
+ 
+	},
+	{
+  
+		fname: '长孙初敏', tname: '令狐叔白'
+ 
+	},
+	{
+  
+		fname: '龙求春', tname: '洪凌志'
+ 
+	},
+	{
+  
+		fname: '百里雪烟', tname: '西门雅槐'
+ 
+	},
+	{
+  
+		fname: '乔亦和', tname: '梅听蕾'
+ 
+	},
+	{
+  
+		fname: '东方丹之', tname: '陈明哲'
+ 
+	},
+	{
+  
+		fname: '柳华真', tname: '苗映岳'
+ 
+	},
+	{
+  
+		fname: '张痴斌', tname: '周紫兰'
+ 
+	},
+	{
+  
+		fname: '关灵夜', tname: '霍留冰'
+ 
+	},
+	{
+  
+		fname: '赵飞灵', tname: '宋元邦'
+ 
+	},
+	{
+  
+		fname: '裴火山', tname: '柯元枫'
+ 
+	},
+	{
+  
+		fname: '梁又爵', tname: '程千阳'
+ 
+	},
+	{
+  
+		fname: '包凡易', tname: '燕真怡'
+ 
+	},
+	{
+  
+		fname: '冯健空', tname: '沈天翠'
+ 
+	},
+	{
+  
+		fname: '滕曼国', tname: '花以雪'
+ 
+	},
+	{
+  
+		fname: '许惜山', tname: '蔡才轩'
+ 
+	},
+	{
+  
+		fname: '冷子丝', tname: '司马尘天'
+ 
+	},
+	{
+  
+		fname: '吕苍佑', tname: '夏语凡'
+ 
+	},
+	{
+  
+		fname: '荆碧雁', tname: '夏侯伯寒'
+ 
+	},
+	{
+  
+		fname: '杜百芹', tname: '熊逸强'
+ 
+	},
+	{
+  
+		fname: '上官义风', tname: '公孙绮菡'
+ 
+	},
+	{
+  
+		fname: '魏天露', tname: '关灵夜'
+ 
+	},
+	{
+  
+		fname: '端木醉元', tname: '赵飞灵'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '裴火山'
+ 
+	},
+	{
+  
+		fname: '尉迟小卉', tname: '梁又爵'
+ 
+	},
+	{
+  
+		fname: '白巨炎', tname: '包凡易'
+ 
+	},
+	{
+  
+		fname: '长孙初敏', tname: '冯健空'
+ 
+	},
+	{
+  
+		fname: '龙求春', tname: '滕曼国'
+ 
+	},
+	{
+  
+		fname: '百里雪烟', tname: '许惜山'
+ 
+	},
+	{
+  
+		fname: '乔亦和', tname: '冷子丝'
+ 
+	},
+	{
+  
+		fname: '东方丹之', tname: '吕苍佑'
+ 
+	},
+	{
+  
+		fname: '柳华真', tname: '荆碧雁'
+ 
+	},
+	{
+  
+		fname: '张痴斌', tname: '杜百芹'
+ 
+	},
+	{
+  
+		fname: '张痴斌', tname: '上官义风'
+ 
+	},
+	{
+  
+		fname: '魏天露', tname: '龙求春'
+ 
+	},
+	{
+  
+		fname: '端木醉元', tname: '百里雪烟'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '乔亦和'
+ 
+	},
+	{
+  
+		fname: '尉迟小卉', tname: '东方丹之'
+ 
+	},
+	{
+  
+		fname: '白巨炎', tname: '柳华真'
+ 
+	},
+	{
+  
+		fname: '长孙初敏', tname: '张痴斌'
+ 
+	},
+	{
+  
+		fname: '魏天露', tname: '尉迟小卉'
+ 
+	},
+	{
+  
+		fname: '端木醉元', tname: '白巨炎'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '长孙初敏'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '魏天露'
+ 
+	},
+	{
+  
+		fname: '汪诗悔', tname: '端木醉元'
+ 
+	},
+
+
+];
---
+++ spe/xcgt/static/scripts/main.js
@@ -0,0 +1,307 @@
+﻿
+var loader = new preloader({
+	resources : [
+		'bc.png',
+		'bg1.jpg', 'bg2.jpg', 'bg3.jpg',
+		'btn1.png', 'btn2.png',
+		'cdfer.png',
+		'd1.png', 'd2.png', 'd3.png',
+		'h1.png', 'h2.png', 'h3.png', 'h4.png',
+		'hp.png', 'i1.png', 'map.png', 
+		'p1.png', 'p2.png', 'p3.png', 'p4.png',
+		's1.png', 's2.png', 's3.png', 's4.png', 's5.png',
+		'start_btn.png', 'title.png', 'title1.png',
+		'xian_title.png', 'zb.png', 'save.png'
+
+	],
+	baseUrl: 'static/images/',
+	onStart : function(total){
+		console.log('start:'+total);
+		Gl.initSize();
+	},
+	onProgress : function(current, total){
+		var percent = current/total*100;
+		$('.progressbar').css('width', percent+'%');
+		$('.progresstext').text(percent +'%');
+		// $('.progresstext .current').text(current);
+		// $('.progresstext .total').text(total);
+	},
+	onComplete : function(total){
+		console.log('加载完毕:'+total+'个资源');
+		init();
+	}
+});
+
+
+
+
+var Gl = {
+	current: null,
+	currentIdx: 100,
+	leaveTime: null,
+	dyingTime: null,
+	remoteImg: null,
+	initSize: function(){
+		var dpr = 1,
+            w = (document.documentElement.clientWidth || document.body.clientWidth)*dpr,
+            h = (document.documentElement.clientHeight|| document.body.clientHeight)*dpr;
+
+        $('#container').css({
+            width: h,
+            height: w,
+            'margin-left': -h/2,
+            'margin-top': -w/2
+        });
+	},
+	getHtml: function(el, rep){
+		var html = el.html();
+		if(rep){
+			var reg = new RegExp("\\[([^\\[\\]]*?)\\]", 'igm');
+			html = html.replace(reg, function (node, key) { 
+				return { 'score': rep.score?rep.score:0, 
+				'src': rep.src,
+				'content': rep.content, 'id': rep.id}[key]; 
+			});
+		}
+		return html;
+	},
+	djs: function(){
+		var num = 0;
+		(function tt(){
+			num = Math.floor(configMap.page3.find('i').text()) - 10;
+			configMap.page3.find('i').text( num );
+			if( num > 0 ){
+				Gl.leaveTime = setTimeout(tt, 1000);
+			}else{
+				clearTimeout(Gl.leaveTime);
+				configMap.mainPage.html(Gl.getHtml(configMap.page4Tpl));
+				configMap.nearby = $('.nearby');
+				configMap.page4 = $('.page-4');
+			}
+		})();
+	},
+	showResult: function(tips){
+		clearTimeout(Gl.leaveTime);
+		clearTimeout(Gl.dyingTime);
+		$.post("http://wx.hxpic.com/spe/2017xunqin/index.php?action=poster", { "rank": Gl.currentIdx },
+	      	function(data){
+	      		Gl.remoteImg = data;
+	      		configMap.showbox.html(Gl.getHtml(configMap.resultTpl, {
+					score: Gl.currentIdx,
+					content: tips
+				}));
+	    });
+	},
+	dying: function(){
+		var bar = $('.progressbar'),
+			txt = $('.progresstxt'),
+			initNum;
+
+		(function dy(){
+			initNum = Math.floor(txt.text().split('/')[0]) - 5;
+
+			if( initNum > -1 ){
+				bar.width(initNum+'%');
+				txt.text(initNum+'/100');
+				Gl.dyingTime = setTimeout(dy, 1000);
+			}else{
+				Gl.showResult('大吉大利，再接再厉！');
+			}
+		})();
+	},
+	getListr: function(data, idx){
+		return '<li>'+ data.fname +' <img src="static/images/i1.png" alt="i1"> '+ data.tname +' - 第'+ idx +'名产生</li>';
+	},
+	loopData: function(){
+		var noticeList = $('.notice-list'), num = 0,
+			lh = $('.notice-box').height()/2;
+
+		(function tt(){
+			num++;
+			Gl.currentIdx = 100-(num+1);
+			
+			noticeList.append(Gl.getListr(mockdata[num], Gl.currentIdx));
+			if(num>2){
+				noticeList.animate({
+					scrollTop: -lh
+				}, 500, function(){
+					noticeList.find('li:first-child').remove();
+					configMap.wi1.text(Gl.currentIdx);
+				});
+			}
+			if( Gl.currentIdx == 1 ){
+				Gl.showResult('大吉大利，今晚吃鸡！');
+			}
+			Gl.leaveTime = setTimeout(tt, 500);
+		})();
+
+	},
+	eating: function(){
+		var img = $('.eat-box img'),
+			el = $('.eat-box .mask'),
+			num, t;
+
+		el.removeClass('hide').text(4);
+		var addnum = Math.floor(img.attr('id')),
+			temp = Math.floor($('.progresstxt').text().split('/')[0]) + addnum;
+		temp = temp > 100 ? 100 : temp;
+		$('.progress').append('<i>+'+ addnum +'</i>');
+		$('.progressbar').width(temp + '%');
+		$('.progresstxt').text(temp + '/100');
+		clearTimeout(Gl.dyingTime);
+		Gl.dying();
+		(function eat(){
+			num = Math.floor(el.text()) - 1
+			if(num>0){
+				el.text(num);
+				t = setTimeout(eat, 1000);
+			}else{
+				el.addClass('hide');
+				img.addClass('hide').attr('id', '');
+				$('.progress i').remove();
+				clearTimeout(t);
+			}
+		})();
+	},
+	person: function(){
+		var t, hero = $('.hero'), n = 1;
+		(function h(){
+			hero.attr('src','static/images/h'+ n +'.png');
+			if(n<5){
+				t = setTimeout(h, 200);
+			}else {
+				clearTimeout(t);
+				hero.attr('src','static/images/h1.png');
+			}
+			n++;
+		})();
+	}
+	
+}
+
+var init,
+	configMap = {
+		mainPage: $('.main-page'),
+		page1Tpl: $('#page1'),
+		page2Tpl: $('#page2'),
+		page3Tpl: $('#page3'),
+		page4Tpl: $('#page4'),
+		confirmTpl: $('#comfirmTpl'),
+		resultTpl: $('#resultTpl'),
+		noticeTpl: $('#noticeTpl'),
+	};
+
+init = function(){
+	$('.prepare-page').remove();
+	configMap.mainPage.html(Gl.getHtml(configMap.page1Tpl))
+	.delegate('.start-btn', 'click', function(){
+		configMap.mainPage.html(Gl.getHtml(configMap.page2Tpl));
+	});
+
+	configMap.mainPage.delegate('.cc a', 'click', function(){
+		var html;
+		 Gl.current = $(this);
+		configMap.page2 = $('.page-2');
+		if(Gl.current.hasClass('hj')){
+			html = Gl.getHtml(configMap.confirmTpl, {
+				content: '由于忘带降落伞，此次行动不能坐飞机。',
+				id: 0
+			});
+		}else{
+			html = Gl.getHtml(configMap.confirmTpl, {
+				content: '确认选择<em>高铁</em>？',
+				id: 1
+			});
+		}
+		
+		configMap.page2.append(html);
+		configMap.confirmMask = configMap.page2.find('.mask');
+	})
+	.delegate('.confirm-btn', 'click', function(){
+		var status = $(this).attr('dataid');
+		if( status == 0 ){
+			configMap.confirmMask.remove();
+		}else{
+			configMap.mainPage.html(Gl.getHtml(configMap.page3Tpl));
+			configMap.page3 = $('.page-3');
+			Gl.djs();
+		}
+	});
+	configMap.mainPage.delegate('.page-3 .title', 'click', function(){
+		clearTimeout(Gl.leaveTime);
+		configMap.mainPage.html(Gl.getHtml(configMap.page4Tpl));
+		configMap.nearby = $('.nearby');
+		configMap.page4 = $('.page-4');
+	})
+	.delegate('.show-box .tips', 'click', function(){
+		Gl.current = $(this);
+		if(Gl.current.hasClass('clicked')){
+			$('.tips-mask').remove();
+			$('.start-tip').removeClass('hide');
+		} else {
+			Gl.current.addClass('clicked').html('但由于过度饥饿，你已无法前进<br>毒气正在蔓延...');
+		}
+		
+	})
+	.delegate('.start-tip img', 'click', function(){
+		Gl.current = $(this);
+		var idx = Math.floor(Gl.current.attr('class').substr(2)) + 1;
+		if( idx > 3 ){ 
+			Gl.current.remove();
+			$('.page-4').addClass('bling');
+			$('.show-box').prepend(Gl.getHtml(configMap.noticeTpl));
+			configMap.wi1 = $('.wi-1');
+			configMap.showbox = $('.show-box');
+			Gl.dying();
+			Gl.loopData();
+
+			configMap.mainPage.delegate('.nearby li', 'click', function(){
+				var mask = $('.eat-box .mask'),
+					img = $('.eat-box img');
+				Gl.current = $(this);
+				
+				if(img.attr('id') != ''){
+					return false;
+				}
+				Gl.person();
+				img.attr({
+					'src': Gl.current.find('img').attr('src'),
+					'id': Gl.current.find('img').attr('id')
+				}).removeClass('hide');
+				if(configMap.nearby.find('li').length>1){
+					Gl.current.remove();
+				}else{
+					configMap.nearby.remove()
+				}
+				
+			})
+			.delegate('.eat-box', 'click', function(){
+				Gl.eating();
+			});
+
+			return false; 
+		}
+		Gl.current.attr({
+			'class': 'st'+idx,
+			'src': 'static/images/d'+ idx +'.png'
+		})
+	});
+
+	configMap.mainPage.delegate('.share-btn', 'click', function(){
+		console.log('share!!!');
+		var _this = $(this);
+		if(_this.hasClass('disabled')){
+			return false;
+		}
+		_this.text('生成中···').addClass('disabled');
+		configMap.resultBox = $('.result-box');
+		configMap.resultBox.load( Gl.remoteImg ,function(){
+			console.log('loadded!!');
+			_this.removeClass('disabled');
+			configMap.resultBox.html('<img class="share-img" src="'+ Gl.remoteImg +'" alt="图片" /><img class="save-img" src="static/images/save.png" alt="save" />');
+		});
+	});	
+}
+
+loader.start();
+
